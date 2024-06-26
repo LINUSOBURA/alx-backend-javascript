3f58ec7114e6439cd43fd2f6cae1296b56197717
LINUSOBURA <linusobura@gmail.com>
2024-06-26 18:29:40 +0300

    Fix errors

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,14 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({ status: 200, body: 'success' });
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'success',
+      });
     } else {
-      reject(new Error('The fake API is not working currently'));
+      reject(new Error(
+        'The fake API is not working currently',
+      ));
     }
   });
 }
