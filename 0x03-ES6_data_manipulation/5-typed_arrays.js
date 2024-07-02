206ecf340ffb4bac82db96f5114deda0d8e2d853
LINUSOBURA <linusobura@gmail.com>
2024-07-02 17:07:57 +0300

    Fixed 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const newBuffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(newBuffer);
-  try {
-    int8View[position] = value;
-  } catch (error) {
-    console.error('Position outside range');
+  const int8View = new DataView(newBuffer, 0, length);
+
+  if (position >= length) {
+    throw Error('Position outside rnge');
   }
+  int8View.setInt8(position, value);
 
   return int8View;
 }
