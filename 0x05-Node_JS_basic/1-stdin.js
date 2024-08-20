eaf5dffdfa9bc03900d08b3b5585674ae49c0b29
LINUSOBURA <linusobura@gmail.com>
2024-08-20 11:31:21 +0300

    Fixed 1-stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,6 +6,7 @@ const rl = readline.createInterface({ input, output });
 
 rl.question('Welcome to Holberton School, what is your name? ', (answer) => {
   console.log(`Your name is: ${answer}`);
+  rl.close();
 });
 
 rl.on('close', () => {
