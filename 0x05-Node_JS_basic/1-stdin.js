c209b4ebd8bf100aaa66ab58a495728d14167bde
LINUSOBURA <linusobura@gmail.com>
2024-08-20 11:51:45 +0300

    Updated 1-std

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
 const readline = require('readline');
+const process = require('process');
 
 const { stdin: input, stdout: output } = require('process');
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('Welcome to Holberton School, what is your name? ', (answer) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
   console.log(`Your name is: ${answer}`);
   rl.close();
 });
