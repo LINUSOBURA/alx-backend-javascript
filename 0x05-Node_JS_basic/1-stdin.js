5149dc1ad237b4d361904780566126b2f31534ad
LINUSOBURA <linusobura@gmail.com>
2024-08-20 11:28:59 +0300

    Initial commit

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+
+rl.question('Welcome to Holberton School, what is your name? ', (answer) => {
+  console.log(`Your name is: ${answer}`);
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});
