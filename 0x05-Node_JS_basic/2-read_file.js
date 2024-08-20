dc1b3ce17ed508542bb29129dbe47f8c51e6319a
LINUSOBURA <linusobura@gmail.com>
2024-08-20 20:58:33 +0300

    Implemented 2-read_file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,16 +4,23 @@ function countStudents() {
   try {
     const data = fs.readFileSync('database.csv', 'utf-8');
     const rows = data.split('\n');
-    const studentCount = rows.filter((row) => row.trim() !== '').length - 1;
-    console.log(`Number of students: ${studentCount}`);
+    const studentRows = rows.slice(1);
+    console.log(`Number of students: ${studentRows.length}`);
 
-    const studentsInCS = rows.filter((row) => row.endsWith('CS'));
-    const firtWords = studentsInCS.map((row) => row.split(',')[0]);
-    console.log(`Number of students in CS ${studentsInCS.length}. List: ${firtWords}`);
+    const fields = {};
+    studentRows.forEach((row) => {
+      const [firstname, , , field] = row.split(',');
+      if (fields[field]) {
+        fields[field].push(firstname);
+      } else {
+        fields[field] = [firstname];
+      }
+    });
 
-    const studentsInSWE = rows.filter((row) => row.endsWith('SWE'));
-    const firstWordsSW = studentsInSWE.map((row) => row.split(',')[0]);
-    console.log(`Number of students in SWE ${studentsInSWE.length}. List: ${firstWordsSW}`);
+    // eslint-disable-next-line guard-for-in
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
   } catch (error) {
     console.error('Cannot load the database');
   }
