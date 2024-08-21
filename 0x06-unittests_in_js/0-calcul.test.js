e763f19297d1290640bb1f23803684abc820cf86
LINUSOBURA <linusobura@gmail.com>
2024-08-21 21:26:56 +0300

    Initial commit

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,10 @@
 const assert = require("assert");
 const calculateNumber = require("../0x06-unittests_in_js/0-calcul");
 
-const result = calculateNumber(4.5 + 5);
+const result1 = calculateNumber(4.5 + 5);
 
 describe("Calculator", function () {
   it("Should round the inputs and return sum", function () {
-    assert.strictEqual(result, Math.round(result));
+    assert.strictEqual(result1, Math.round(result1));
   });
 });
