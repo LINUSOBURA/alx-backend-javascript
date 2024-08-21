91c90442f61fb7dee800922d5b8304f0355daf01
LINUSOBURA <linusobura@gmail.com>
2024-08-21 22:54:58 +0300

    Implemeted 1-calcul and test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,24 +4,18 @@ const { describe, it } = require("node:test");
 
 describe("Calculator", function () {
   it("Should round the inputs and return sum", function () {
-    assert.strictEqual(
-      calculateNumber("SUM", 1.4, 4.5),
-      Math.round(1.4) + Math.round(4.5)
-    );
+    assert.strictEqual(calculateNumber("SUM", 1.4, 4.5), 6);
   });
 
   it("Should round off inputs and return the difference", function () {
-    assert.strictEqual(
-      calculateNumber("SUBTRACT", 1.4, 4.5),
-      Math.round(1.4) - Math.round(4.5)
-    );
+    assert.strictEqual(calculateNumber("SUBTRACT", 1.4, 4.5), -4);
   });
 
   it("Should round off inputs and return the quotient", function () {
-    assert.strictEqual(
-      calculateNumber("DIVIDE", 1.4, 4.5),
-      Math.round(1.4) / Math.round(4.5)
-    );
-    assert.equal(calculateNumber("DIVIDE", 1.4, 0), "Error");
+    assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
+  });
+  it("Should return 'Error' when dividing by zero", function () {
+    assert.strictEqual(calculateNumber("DIVIDE", 1.4, 0), "Error");
+    assert.strictEqual(calculateNumber("DIVIDE", 1.4, 0.2), "Error");
   });
 });
